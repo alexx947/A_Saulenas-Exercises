d1679b49ccee1b97010de9b7580c09c0cf75c536
Aleksas Saulenas <aleksassaulenas@gmail.com>
2023-10-12 14:59:10 +0300

    PD5

---
+++ PD5/js/main.js
@@ -0,0 +1,50 @@
+let mynumber = '1 num';
+let mybigint = '2 bigint';
+let mystring = '3 string';
+let myboolean = '4 boolean';
+let myundefined = undefined;
+let mynull = null;
+let myobject = '7 object';
+
+
+console.log(mynumber);
+console.log(mybigint);
+console.log(mystring);
+console.log(myboolean);
+console.log(myundefined);
+console.log(mynull);
+console.log(myobject);
+
+
+let number = 12;
+console.log(typeof number);
+
+let otherNumber = 45.89;
+console.log(typeof otherNumber);
+
+let textOne = 'Beautiful JavaScript World';
+console.log(typeof textOne);
+
+let textTwo = "Beautiful JavaScript World";
+console.log(typeof textTwo);
+
+let textThree = `Beautiful JavaScript World`;
+console.log(typeof textThree);
+
+let isTrue = (true == false);
+console.log(typeof isTrue);
+
+let isEqual = (2 >= 0);
+console.log(typeof isEqual);
+
+let petName = undefined;
+console.log(typeof petName);
+
+let petAge = null;
+console.log(typeof petAge);
+
+let human = {
+    humanName: undefined,
+    humanAge: null
+};
+console.log(typeof human);
