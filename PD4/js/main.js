1f299dd54d300bda2d1d36d130a6547f9b5b6289
Aleksas Saulenas <aleksassaulenas@gmail.com>
2023-10-12 13:34:43 +0300

    PD4

---
+++ PD4/js/main.js
@@ -0,0 +1,14 @@
+let petName;
+let petAge;
+
+petName = 'Jon';
+petAge = 40;
+
+console.log(petName);
+console.log(petAge);
+
+petName = 'Binky';
+petAge = 14;
+
+const buildingAddress = 'Trinapolio g. 2';
+console.log(buildingAddress);
